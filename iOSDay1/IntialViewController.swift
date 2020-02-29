2a998ef82b51e794caba928b31240f81c637f524
Anmol Singh <anmolsingh1597@gmail.com>
2020-02-28 20:56:47 -0500

    [Anmol Singh]Intial Commit 1.1

--- iOSDay1/IntialViewController.swift
+++ iOSDay1/IntialViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class IntialViewController: UIViewController {
 
+    @IBOutlet weak var intialConnection: UILabel!
+    
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        intialConnection.text = "Welcome"
+       print(intialConnection.text!)
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    @IBAction func iButton(_ sender: UIButton) {
+        intialConnection.text = "Let get this started!!"
+    }
+    
+    @IBAction func iTextField(_ sender: UITextField) {
+        
+    }
+    
 
 }
 
