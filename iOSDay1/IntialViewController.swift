7341c244074c8d628bf15d36efc7163a7398cba4
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-05 13:16:29 -0500

    [Anmol Singh]Firebase: Data Retrieved and Mapped Successfully

--- iOSDay1/IntialViewController.swift
+++ iOSDay1/IntialViewController.swift
@@ -45,7 +45,7 @@ class IntialViewController: UIViewController {
     
     @IBAction func iButton(_ sender: UIButton) {
         
-        // assign tag to button in button attributes inspector
+// assign tag to button in button attributes inspector
           if sender.tag == 0 //Show alert- alert in front
           {
           let alertController = UIAlertController(title: "Error", message:
@@ -61,36 +61,49 @@ class IntialViewController: UIViewController {
            
           self.present(alertController, animated: true, completion: nil)
           }
-           // assign tag to button in button attributes inspector
+// assign tag to button in button attributes inspector
           if sender.tag == 1 //show action- pop up from bottom
           {
             let alertController = UIAlertController(title: "Action", message:
               "Tasks", preferredStyle: .actionSheet)
             alertController.addAction(UIAlertAction(title: "Add in", style: .default, handler: {(sender) in
+// Data Insertion in Firebase
                 let user = self.iText.text
                 let password = self.iPassword.text
+// Generation of AutoKey in Firebase using "childByAutoId()"
                guard let key = self.ref.child("Users").childByAutoId().key else { return } // value assign in firebase through alert in "Sign in Key"
                        let insert = ["userName": user,
                                    "password": password]
                        let childUpdates = ["/Users/\(key)": insert]
+// values inserted successfully
                 self.ref.updateChildValues(childUpdates)
             }))
             alertController.addAction(UIAlertAction(title: "Sign in", style: .destructive, handler: {(sender) in
+// Retrieveing data from Firebase
                 let refer = self.ref.child("Users")
-            //   let user = self.iText.text as Any
-             //  let password = self.iPassword.text
-             // var userData: Dictionary<String,Any>
+// Taking snapshot of saved data in Firebase
                 refer.observeSingleEvent(of: .value, with: { (snapshot) in
-                          if let userDict = snapshot.value as? [String:Any] {
-                               //Do not cast print it directly may be score is Int not string
-                          //  userData = userDict.values
-                            print(userDict.values)
-                         //   if userDict.values.contains(user)
+                    if let userDict = snapshot.value as? [String:[String:String]] {
+                        print(userDict.values)
+// Matching the user inputted values with already stored ones(in Firebase)
+                        let userData = ["userName": self.iText.text, "password": self.iPassword.text]
+// if mapped - then show alert "Successfully Logged In!"
+                        if userDict.values.contains(userData as! [String : String]){
+                            print("It worked")
+                            let alertControll = UIAlertController(title: "Successfully Logged In!", message: "Logged In", preferredStyle: .alert)
+                             alertControll.addAction(UIAlertAction(title: "Ok", style: .default))
+                             self.present(alertControll, animated: true, completion: nil)
+// if unmatched- then show alert "Log In Failed"
+                        }else{
+                            print("Still not working")
+                            let alertControll = UIAlertController(title: "Log In Failed", message: "Invalid User-name and Password ", preferredStyle: .alert)
+                            alertControll.addAction(UIAlertAction(title: "Ok", style: .default))
+                            self.present(alertControll, animated: true, completion: nil)
+                        }
                     }
                      })
             }))
             alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-             // alertController.addAction(UIAlertAction(title: "default2", style: .default))
            /*alertController.addAction(UIAlertAction(title: "Destructive2", style: .destructive, handler: { (sender) in
                 print("Click Destructive")}))*/
              self.present(alertController, animated: true, completion: nil)
@@ -112,11 +125,12 @@ class IntialViewController: UIViewController {
 //            intialConnection.text = "Login Failed"
 //        }
         
-    //update in database as dictionary
-  /*  let user = iText.text
+//update in database as dictionary
+        
+    /*  let user = iText.text
     let password = iPassword.text
-        guard let key = ref.child("Users").childByAutoId().key else { return }
-        let insert = ["userName": user,
+    guard let key = ref.child("Users").childByAutoId().key else { return }
+    let insert = ["userName": user,
                     "password": password]
         let childUpdates = ["/Users/\(key)": insert]
         ref.updateChildValues(childUpdates)
